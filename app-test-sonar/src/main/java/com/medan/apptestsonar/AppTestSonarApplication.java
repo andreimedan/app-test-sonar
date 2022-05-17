c35251257a99781b92aed96638cc6d6c61f2db1d
Marian Matea <marian.matea@raro.eu>
2022-05-17 17:01:11 +0300

    Initial commit

---
+++ app-test-sonar/src/main/java/com/medan/apptestsonar/AppTestSonarApplication.java
@@ -0,0 +1,14 @@
+package com.medan.apptestsonar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppTestSonarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppTestSonarApplication.class, args);
+		System.out.println("Up!!!");
+	}
+
+}
---
+++ app-test-sonar/src/test/java/com/medan/apptestsonar/AppTestSonarApplicationTests.java
@@ -0,0 +1,13 @@
+package com.medan.apptestsonar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppTestSonarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
